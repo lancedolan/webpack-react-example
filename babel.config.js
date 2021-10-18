5047b7f96d16c1e06ed990c95fa9a60c7d404dc0
lancedolan <lance.dolan@gmail.com>
2021-10-18 13:47:21 -0500

    solved regression in jest unit tests. Committed prod build to dist instead of dev build

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
 /*
  * This config isn't used when building for production.
- * This allows Jest to babel transpile source when running Unit tests, and thus test ES export packages.
+ * This allows Jest to babel-transpile source when running Unit tests, and thus test ES export packages.
  *
- * This project's actual build-configs for babel are found in the webpack config.
+ * Babel configs use during actual app builds are found in the webpack configs.
  */
 module.exports = {
     presets: [['@babel/preset-env', {targets: {node: 'current'}}], '@babel/preset-react'],
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,20 @@
 module.exports = {
+    /**
+     * Mock front-end assets which are imported by our dependencies but have no value in unit tests.
+     */
     "moduleNameMapper": {
         "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/style-mock.js",
         "\\.(scss|less)$": "<rootDir>/test/style-mock.js"
-    }
+    },
+    /**
+     * mock Browser API, since this is a webapp and our dependencies assume global Browser API
+     */
+    "testEnvironment": "jsdom",
+    /**
+     * As a rule, node_modules are already transpiled.
+     * Some libraries break that rule, so we must transpile them during jest, which depends on commonJS.
+     */
+    "transformIgnorePatterns": [
+        "<rootDir>/node_modules/(?!lodash-es)"
+    ]
 }
